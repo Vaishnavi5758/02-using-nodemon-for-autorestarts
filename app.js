34ca73e8074da6419067ac37bf4303311db4cdba
Vaishnavi5758 <vaishnavipatil5758@gmail.com>
2023-10-31 23:33:58 +0530

    intro to middleware

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app=express();
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log("1");
-    res.send('<h1>Hello</h1>');
-    //next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send( '<form action="/product" method="POST">' +
+  '<input type="text" name="title" placeholder="Product Title">' +
+  '<input type="text" name="size" placeholder="Product Size">' +
+  '<button type="submit">Add Product</button>' +
+  '</form>');
 });
 
-app.use((req,res,next)=>{
-    console.log("2");
-   // next;
+app.use('/product', (req, res, next) => {
+    const title = req.body.title;
+    const size = req.body.size;
+    console.log('Product Title:', title);
+    console.log('Product Size:', size);
+    res.redirect('/');
 });
 
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-app.listen(4000);
+app.listen(3000);
